804d2d620520293d58ef37f6dc4ca23ba48a9c2a
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-23 15:29:02 +0530

    addint

--- Aug23Exercises/Program.cs
+++ Aug23Exercises/Program.cs
@@ -4,57 +4,19 @@
     {
         static void Main(string[] args)
         {
-            students[] arr = new students[4];
-            int k;
-            string bookname;
-            for(int i=0;i<arr.Length; i++)
-            {
-                Console.WriteLine("Enter RollNo");
-                arr[i].rollno=Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter studentname");
-                arr[i].StudentName = Console.ReadLine();
-                
-            }
-            Console.WriteLine("Enter the details");
-            for(int i = 0; i < 4; i++)
-            {
-                arr[i].display();
-            }
+            int a, b;
+            Console.WriteLine("Enter Numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int c;
+            c = addition(a, b);
+            Console.WriteLine("Sum={0}", c);
             Console.ReadLine();
-            
-                
-            
         }
-    }
-    struct students
-    {
-        public int RollNo;
-        public string Student;
-        public int rollno
+        static int addition(int c, int k)
         {
-            set
-            {
-                RollNo = value;
-            }
-            get
-            {
-                return RollNo;
-            }
+            return (c + k);
+
         }
-        public string StudentName
-        {
-            set
-            {
-                Student = value;
-            }
-            get
-            {
-                return Student;
-            }
-        }
-       public void display()
-        {
-            Console.WriteLine("{0} {1}\n",RollNo,Student);
-        } 
     }
 }
