88ac7838fd0dcc44bc0cc5604f1857b0151e2443
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-23 16:26:30 +0530

    divideby0

--- Aug23Exercises/Program.cs
+++ Aug23Exercises/Program.cs
@@ -4,57 +4,21 @@
     {
         static void Main(string[] args)
         {
-            students[] arr = new students[4];
-            int k;
-            string bookname;
-            for(int i=0;i<arr.Length; i++)
+            int i, j;
+            Console.WriteLine("Enter Numbers");
+            i=Convert.ToInt32(Console.ReadLine());
+            j=Convert.ToInt32(Console.ReadLine());
+            try
             {
-                Console.WriteLine("Enter RollNo");
-                arr[i].rollno=Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter studentname");
-                arr[i].StudentName = Console.ReadLine();
-                
+                int k = i / j;
+                Console.WriteLine(k);
             }
-            Console.WriteLine("Enter the details");
-            for(int i = 0; i < 4; i++)
+            catch(DivideByZeroException ex)
             {
-                arr[i].display();
+                Console.WriteLine(ex.StackTrace);
+
             }
             Console.ReadLine();
-            
-                
-            
-        }
-    }
-    struct students
-    {
-        public int RollNo;
-        public string Student;
-        public int rollno
-        {
-            set
-            {
-                RollNo = value;
-            }
-            get
-            {
-                return RollNo;
-            }
-        }
-        public string StudentName
-        {
-            set
-            {
-                Student = value;
-            }
-            get
-            {
-                return Student;
-            }
-        }
-       public void display()
-        {
-            Console.WriteLine("{0} {1}\n",RollNo,Student);
         } 
     }
 }
