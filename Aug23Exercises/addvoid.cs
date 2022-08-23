0a4fa4fed629e348981f39b309b2be10ee795f82
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-23 15:22:09 +0530

    Add project files.

---
+++ Aug23Exercises/addvoid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aug23Exercises
+{
+    internal class addvoid
+    {
+        static void main(string[] args) {
+            int a, b;
+            Console.WriteLine("Enter Numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int c;
+            c = addition(a,b);
+            Console.WriteLine("Sum={0}",c);
+            Console.ReadLine();
+        }
+        static int addition(int c,int k)
+        {
+            return(c + k);
+           
+        }
+           
+    }
+}
